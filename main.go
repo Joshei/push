d6411fec490df09b7c79048cd8b0b563c46a9984
Joshua Eirman <joshuaeirm@gmail.com>
2021-09-21 16:43:19 -0400

    this is to ask for help with spitbackamounts

--- main.go
+++ main.go
@@ -324,7 +324,7 @@ func processLogin(w http.ResponseWriter, r *http.Request) {
 
 /////////////////////////////////////////
 ////////////////////////////////////////
-
+ //
 func spitBackAmounts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
